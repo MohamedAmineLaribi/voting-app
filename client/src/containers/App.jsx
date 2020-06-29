d59b32da26805641380af57e1f9a5364519ec780
khaliljedda <khaliljedda4@gmail.com>
2020-06-29 09:12:39 +0100

    fix err

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import decode from "jwt-decode";
-
 import { store } from "../store";
 import { setToken, setCurrentUser, addError } from "../store/actions";
 
